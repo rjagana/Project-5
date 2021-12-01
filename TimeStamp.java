8ddb1182f38653f717491bc18c4246a08b706ac9
rjagana <89652688+rjagana@users.noreply.github.com>
2021-12-01 00:22:35 -0500

    Update TimeStamp.java

--- TimeStamp.java
+++ TimeStamp.java
@@ -4,10 +4,11 @@ import java.util.Date;
 public class TimeStamp
 {
 
-    public static void printTimeStamp() {
+    public static String printTimeStamp() {
         Date date= new Date();
         long currentTime = date.getTime();
         Timestamp ts = new Timestamp(currentTime);
-        System.out.println("Current Time Stamp: "+ts);
+        String timeString = "Current Time Stamp: "+ ts;
+        return timeString;
     }
 }
