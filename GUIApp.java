6f66159276babfbc957917fe0b674200ca3acaa9
rjagana <89652688+rjagana@users.noreply.github.com>
2021-12-01 16:14:48 -0500

    Updates student functions to GUI

--- GUIApp.java
+++ GUIApp.java
@@ -5,6 +5,10 @@ public class GUiApp {
 
     public static void main (String[] args) throws IOException {
         boolean deleteCheck = false;
+        boolean checking = false;
+        boolean check4 = false;
+        String quizTitleChoice = "";
+        int count4 = 0;
         //Welcome Message
         JOptionPane.showMessageDialog(null, "Welcome to the Quiz Application!", "Quiz Application",
                 JOptionPane.INFORMATION_MESSAGE);
@@ -56,31 +60,68 @@ public class GUiApp {
                    JOptionPane.showMessageDialog(null, "Thank you for using the Quiz Application!"
                            , "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
                } else if (userChoice.equals("Edit Account")) {                  //teacher is editing their account
-
+                   String newUsername = JOptionPane.showInputDialog(null, "Enter your new username: ", "Quiz Application",
+                           JOptionPane.QUESTION_MESSAGE);
+                   String newPassword = JOptionPane.showInputDialog(null, "Enter your new password: ", "Quiz Application",
+                           JOptionPane.QUESTION_MESSAGE);
+                   String tempUser = teacher.editUser(teacher.getUsername(), newUsername, newPassword);
+                   teacher.setUsername(tempUser);
+                   JOptionPane.showMessageDialog(null, "User edited successfully!", "Quiz Application",
+                           JOptionPane.INFORMATION_MESSAGE);
                } else if (userChoice.equals("Delete Account")) {                //teacher is deleting their account
-
+                   teacher.deleteUser(teacher.getUsername());
+                   JOptionPane.showMessageDialog(null, "User deleted. Thank you for using the quiz application!",
+                          "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
                }
-           } while (!userChoice.equals("Logout"));
+           } while (!userChoice.equals("Logout") && !userChoice.equals("Delete Account"));
            //if user is a student
        } else if (user == 0) {
-            Student student = new Student();
-            student.runUser("student");
-           String[] choices = {"Take a Quiz", "Logout", "Edit Account", "Delete Account"};
+           Student student = new Student();
+           student.runUser("student");
            String userChoice;
-           userChoice = (String) JOptionPane.showInputDialog(null, "Select which of the following you would like to do",
-                   "Quiz Application", JOptionPane.QUESTION_MESSAGE,
-                   null, choices, choices[0]);
-           if (userChoice.equals("Take a Quiz")) {
 
-           } else if (userChoice.equals("Logout")) {
+           do {
+               String[] choices = {"Take a Quiz", "Logout", "Edit Account", "Delete Account"};
+               userChoice = (String) JOptionPane.showInputDialog(null, "Select which of the following you would like to do",
+                       "Quiz Application", JOptionPane.QUESTION_MESSAGE,
+                       null, choices, choices[0]);
+               if (userChoice.equals("Take a Quiz")) {
+                   do {
+                       quizTitleChoice = student.chooseQuiz();
+                       checking = student.checkMatch(quizTitleChoice);
+                       if (!checking) {
+                           check4 = false;
+                       } else {
+                           JOptionPane.showMessageDialog(null, student.answerQuiz(quizTitleChoice), "Quiz Application",
+                                   JOptionPane.INFORMATION_MESSAGE);
+                           JOptionPane.showMessageDialog(null, student.writeAnswers(quizTitleChoice, student.answerQuiz(quizTitleChoice)),
+                                   "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
+                           check4 = true;
+                       }
+                       JOptionPane.showMessageDialog(null, "The quiz was turned in at\n" + TimeStamp.printTimeStamp(),
+                               "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
+                       count4++;
+                   } while (!check4);
 
-           } else if (userChoice.equals("Edit Account")) {
+               } else if (userChoice.equals("Logout")) {
+                   JOptionPane.showMessageDialog(null, "Thank you for using the Quiz Application!"
+                           , "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
+               } else if (userChoice.equals("Edit Account")) {
+                   String newUsername = JOptionPane.showInputDialog(null, "Enter your new username: ",
+                           "Quiz Application", JOptionPane.QUESTION_MESSAGE);
+                   String newPassword = JOptionPane.showInputDialog(null, "Enter your new password: ",
+                           "Quiz Application", JOptionPane.QUESTION_MESSAGE);
+                   String tempUser = student.editUser(student.getUsername(), newUsername, newPassword);
+                   student.setUsername(tempUser);
+               } else if (userChoice.equals("Delete Account")) {
+                   student.deleteUser(student.getUsername());
+                   JOptionPane.showMessageDialog(null, "User deleted. Thank you for using the quiz application!",
+                           "Quiz Application", JOptionPane.INFORMATION_MESSAGE);
 
-           } else if (userChoice.equals("Delete Account")) {
+               }
 
-           }
-
-        }
+           } while (!userChoice.equals("Logout") && !userChoice.equals("Delete Account"));
+       }
 
     }
 }
