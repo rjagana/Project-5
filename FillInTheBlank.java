3b49a470d5bf40828847ca6928b71670adb24db0
rjagana <89652688+rjagana@users.noreply.github.com>
2021-12-01 00:20:38 -0500

    Update FillInTheBlank to add GUI parts

--- FillInTheBlank.java
+++ FillInTheBlank.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class FillInTheBlank extends Question {
@@ -36,9 +37,28 @@ public class FillInTheBlank extends Question {
         return answer;
     }
 
+    public String setAnswer2() {
+        String choicePrompt = "What is the answer to the blank?";
+        String choiceError = "Please enter some text for your choice!";
+        String answer = "";
+        boolean loop = true;
+        Scanner scn = new Scanner(System.in);
+
+        while (loop) {
+            answer = JOptionPane.showInputDialog(null, choicePrompt, "Quiz Application", JOptionPane.QUESTION_MESSAGE);
+            if (answer != null) {
+                loop = false;
+            } else {
+                System.out.println(choiceError);
+                loop = true;
+            }
+        }
+        return answer;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s\nAnswer: %s\n", getQuestion(), setAnswer());
+        return String.format("%s\nAnswer: %s\n", getQuestion(), setAnswer2());
     }
 
     public String toDataString() {
