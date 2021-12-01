d901f9c93f119b7c7aa2707133eb539723401d98
rjagana <89652688+rjagana@users.noreply.github.com>
2021-12-01 00:21:51 -0500

    Update TrueorFalse to have GUI

--- TrueOrFalse.java
+++ TrueOrFalse.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class TrueOrFalse extends Question {
@@ -37,9 +38,30 @@ public class TrueOrFalse extends Question {
         return answer;
     }
 
+    public String setAnswer2() {
+        String choicePrompt = "Please select the letter answer True (T) or False (F): ";
+        String choiceError = "Please enter T or F!";
+        String answer;
+        boolean loop = true;
+        Scanner scn = new Scanner(System.in);
+
+            String[] options = {"False", "True"};
+            answer = String.valueOf(JOptionPane.showOptionDialog(null, choicePrompt, "Quiz Application",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+                    null, options, options[1]));
+
+            if (answer.equals("1")) {
+                answer = "True";
+            } else {
+                answer = "False";
+            }
+
+        return answer;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s\nAnswer: %s\n", getQuestion(), setAnswer());
+        return String.format("%s\nAnswer: %s\n", getQuestion(), setAnswer2());
     }
 
     public String toDataString() {
